ebaf118647f073de63234e87a0aca743806f49af
tomnatt <github@tomnatt.com>
2014-12-28 19:26:45 +0000

    reindented

--- _plugins/month.rb
+++ _plugins/month.rb
@@ -1,55 +1,55 @@
 class MonthMarkup < Liquid::Tag
 
-    def initialize(tag_name, text, tokens)
-        super
-        params = text.split(" ")
-        @month = params[0]
-        @title_month = @month.slice(0,1).capitalize + @month.slice(1..-1)
-    end
+  def initialize(tag_name, text, tokens)
+    super
+    params = text.split(" ")
+    @month = params[0]
+    @title_month = @month.slice(0,1).capitalize + @month.slice(1..-1)
+  end
 
-    def render(context)
+  def render(context)
 
 <<-eos
-<section class="row month future" id="#{@month}">
+  <section class="row month future" id="#{@month}">
     <h2>#{@title_month}</h2>
     <div class="images">
-        <ul class="polaroids large-block-grid-3 small-block-grid-2">
-            <li>
-                <a title="image placeholder" href="#">
-                    <img alt="placeholder" src="/images/image-placeholder.png">
-                </a>
-            </li>
-            <li>
-                <a title="image placeholder" href="#">
-                    <img alt="placeholder" src="/images/image-placeholder.png">
-                </a>
-            </li>
-            <li>
-                <a title="image placeholder" href="#">
-                    <img alt="placeholder" src="/images/image-placeholder.png">
-                </a>
-            </li>
-            <li>
-                <a title="image placeholder" href="#">
-                    <img alt="placeholder" src="/images/image-placeholder.png">
-                </a>
-            </li>
-            <li>
-                <a title="image placeholder" href="#">
-                    <img alt="placeholder" src="/images/image-placeholder.png">
-                </a>
-            </li>
-            <li>
-                <a title="image placeholder" href="#">
-                    <img alt="placeholder" src="/images/image-placeholder.png">
-                </a>
-            </li>
-        </ul>
+      <ul class="polaroids large-block-grid-3 small-block-grid-2">
+        <li>
+          <a title="image placeholder" href="#">
+            <img alt="placeholder" src="/images/image-placeholder.png">
+          </a>
+        </li>
+        <li>
+          <a title="image placeholder" href="#">
+            <img alt="placeholder" src="/images/image-placeholder.png">
+          </a>
+        </li>
+        <li>
+          <a title="image placeholder" href="#">
+            <img alt="placeholder" src="/images/image-placeholder.png">
+          </a>
+        </li>
+        <li>
+          <a title="image placeholder" href="#">
+            <img alt="placeholder" src="/images/image-placeholder.png">
+          </a>
+        </li>
+        <li>
+          <a title="image placeholder" href="#">
+            <img alt="placeholder" src="/images/image-placeholder.png">
+          </a>
+        </li>
+        <li>
+          <a title="image placeholder" href="#">
+            <img alt="placeholder" src="/images/image-placeholder.png">
+          </a>
+        </li>
+      </ul>
     </div>
-</section>
+  </section>
 eos
-    end
+  end
 
-    Liquid::Template.register_tag "month", self
+  Liquid::Template.register_tag "month", self
 
 end
--- _plugins/picture.rb
+++ _plugins/picture.rb
@@ -1,36 +1,36 @@
 class PictureMarkup < Liquid::Tag
 
-    require 'yaml'
+  require 'yaml'
 
-    def initialize(tag_name, text, tokens)
-        super
-        params = text.split(" ")
-        @image = params[0]
+  def initialize(tag_name, text, tokens)
+    super
+    params = text.split(" ")
+    @image = params[0]
 
-        # read the config from a yml file
-        index = nil
-        data = YAML.load_file('images/_data.yml')['pictures']
-        data.each_with_index do |pic, i|
-            if pic['image'] == @image
-                index = i
-                break
-            end
-        end
-
-        @caption = data[index]['caption']
-        @alt = data[index]['alt']
+    # read the config from a yml file
+    index = nil
+    data = YAML.load_file('images/_data.yml')['pictures']
+    data.each_with_index do |pic, i|
+      if pic['image'] == @image
+        index = i
+        break
+      end
     end
 
-    def render(context)
+    @caption = data[index]['caption']
+    @alt = data[index]['alt']
+  end
+
+  def render(context)
 <<-eos
-<li>
+  <li>
     <a title="#{@caption}" href="#">
-        <img alt="#{@alt}" src="/images/#{@image}">
+      <img alt="#{@alt}" src="/images/#{@image}">
     </a>
-</li>
+  </li>
 eos
-    end
+  end
 
-    Liquid::Template.register_tag "picture", self
+  Liquid::Template.register_tag "picture", self
 
 end
