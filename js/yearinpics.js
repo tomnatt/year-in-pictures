76678f56ea0bdc933c0e33534b32fbb61604c78f
tomnatt <github@tomnatt.com>
2014-12-29 19:30:23 +0000

    added scroll to anchor and fixed foundation js

--- js/yearinpics.js
+++ js/yearinpics.js
@@ -1,3 +1,12 @@
 $(function() {
-    $(document).foundation();
+  $(document).foundation();
 });
+
+$(function() {
+  $(".month-navigation ul li a").click(function() {
+    var target = $(this).attr('href');
+    $("html,body").animate({
+      scrollTop: $(target).offset().top
+    },"1000");
+  })
+})
