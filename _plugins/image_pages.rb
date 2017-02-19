7d1c618eb52d68a8765cabc9900cafe26cb14db5
tomnatt <github@tomnatt.com>
2017-02-19 16:08:24 +0000

    Major refactor of image page plugin and first pass of image nav

--- _plugins/image_pages.rb
+++ _plugins/image_pages.rb
@@ -7,49 +7,78 @@ module ImagePages
       @base = base
       @dir = dir
 
-      f = File.basename(filename, File.extname(filename)) + '.html'
-      @name = f # name of the generated page
+      @name = File.basename(filename, File.extname(filename)) + '.html' # name of the generated page
 
       # the placeholder will have no year
-      @year = (dir.split('/')[1] ? dir.split('/')[1] : '')
+      year = (dir.split('/')[1] ? dir.split('/')[1] : '')
 
       # read the config from a yml file
       index = nil
-      data = YAML.load_file(File.join('images', @year, '_data.yml'))['pictures']
-      data.each_with_index do |pic, i|
+      yaml_data = YAML.load_file(File.join('images', year, '_data.yml'))['pictures']
+      yaml_data.each_with_index do |pic, i|
         if pic['image'] == filename
           index = i
           break
         end
       end
 
-      image_data = data[index]
+      image_data = yaml_data[index]
 
       self.process(@name)
       self.read_yaml(File.join(@base, '_layouts'), 'picture.html')
 
-      if @year == '2017'
-        self.data['title'] = 'Year of the Rooster'
-        self.data['index'] = ''
-      elsif @year == '2016'
-        self.data['title'] = 'Year of the Monkey'
-        self.data['index'] = '2016.html'
-      elsif @year == '2015'
-        self.data['title'] = 'Year of the Sheep'
-        self.data['index'] = '2015.html'
+      self.data = year_specific_data(year)
+      self.data.merge!(picture_specific_data(image_data))
+
+      self.data['months'] = 'false' # omit global month navigation
+      self.data['month'] = image_data['month'] # which month
+
+      # next and previous pictures for navigtion
+      self.data['next'] = yaml_data[next_index(index, yaml_data.length)]['image']
+      self.data['previous'] = yaml_data[previous_index(index)]['image']
+    end
+
+    def next_index(i, max)
+      i += 1
+      return i unless i == max
+      # return 0 if we've hit the end of the data array
+      0
+    end
+
+    def previous_index(i)
+      # array will wrap automatically
+      i -= 1
+    end
+
+    def year_specific_data(year)
+      data = Hash.new
+      data['year'] = year
+
+      if year == '2017'
+        data['title'] = 'Year of the Rooster'
+        data['index'] = ''
+      elsif year == '2016'
+        data['title'] = 'Year of the Monkey'
+        data['index'] = '2016.html'
+      elsif year == '2015'
+        data['title'] = 'Year of the Sheep'
+        data['index'] = '2015.html'
       else
-        self.data['title'] = 'Year of the Unknown'
-        self.data['index'] = ''
+        data['title'] = 'Year of the Unknown'
+        data['index'] = ''
       end
 
-      self.data['months'] = 'false'
-      self.data['month'] = image_data['month']
-      self.data['year'] = @year
-      self.data['image'] = image_data['image']
-      self.data['photographer'] = image_data['image'][/([a-z]+)/, 1]
-      self.data['image_title'] = image_data['image_title']
-      self.data['description'] = image_data['description']
-      self.data['alt'] = image_data['alt']
+      data
+    end
+
+    def picture_specific_data(picture_data)
+      data = Hash.new
+      data['image'] = picture_data['image']
+      data['photographer'] = picture_data['image'][/([a-z]+)/, 1]
+      data['image_title'] = picture_data['image_title']
+      data['description'] = picture_data['description']
+      data['alt'] = picture_data['alt']
+      data
     end
   end
 
@@ -67,7 +96,7 @@ module ImagePages
           if file =~ /.jpg/ || file =~ /.png/
             # omit listed pictures
             unless omit_list.include?(file)
-              new_page =  ImagePage.new(site, Dir.pwd, File.join('photos', year), file)
+              new_page = ImagePage.new(site, Dir.pwd, File.join('photos', year), file)
               site.pages << new_page
             end
           end
