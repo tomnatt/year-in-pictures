71f425b9de0e3771e39ba252ccf612970cb00db4
tomnatt <github@tomnatt.com>
2024-10-29 14:56:24 +0000

    Implement tab indexing with working Year nav

--- js/yearinpics.js
+++ js/yearinpics.js
@@ -1,17 +1,40 @@
 $(function() {
+
   $("a.year-menu-toggle, .year-menu-close").on("click", function() {
     $(".year-menu-toggle").attr("aria-expanded", function (i, attr) {
-        return attr == "true" ? "false" : "true"
+      return attr == "true" ? "false" : "true"
     });
     $(".page-wrap").toggleClass("move-left");
   });
-});
 
-$(function() {
+  $("a.year-menu-toggle, .year-menu-close").on("keypress", function(e) {
+    if(e.key == "Enter") {
+      $(".year-menu-toggle").attr("aria-expanded", function (i, attr) {
+        return attr == "true" ? "false" : "true"
+      });
+      $(".page-wrap").toggleClass("move-left");
+    }
+  });
+
+  // $(".year-menu aside ul li a").on("focus", function() {
+  $(".year-menu-toggle").on("focus", function() {
+    $(".year-menu-toggle").attr("aria-expanded", function (i, attr) {
+      return attr == "true" ? "false" : "true"
+    });
+    $(".page-wrap").toggleClass("move-left");
+  });
+
+  $('[TabIndex="30"]').on("focus", function() {
+    $(".year-menu-toggle").attr("aria-expanded", function (i, attr) {
+      return attr == "true" ? "false" : "true"
+    });
+    $(".page-wrap").toggleClass("move-left");
+  });
+
   $(".month-navigation ul li a").click(function() {
     var target = $(this).attr("href");
     $("html,body").animate({
       scrollTop: $(target).offset().top
     },"1000");
-  })
-})
+  });
+});
