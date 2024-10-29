5652abd18b6818e419f6c67b394ab66a71b297a2
tomnatt <github@tomnatt.com>
2024-10-29 14:16:46 +0000

    Fix correct aria property for year menu

--- js/yearinpics.js
+++ js/yearinpics.js
@@ -1,12 +1,15 @@
 $(function() {
   $("a.year-menu-toggle, .year-menu-close").on("click", function() {
+    $(".year-menu-toggle").attr("aria-expanded", function (i, attr) {
+        return attr == "true" ? "false" : "true"
+    });
     $(".page-wrap").toggleClass("move-left");
   });
 });
 
 $(function() {
   $(".month-navigation ul li a").click(function() {
-    var target = $(this).attr('href');
+    var target = $(this).attr("href");
     $("html,body").animate({
       scrollTop: $(target).offset().top
     },"1000");
