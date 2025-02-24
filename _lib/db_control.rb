f0e958f28d8c37ade659e721c25f42da67f60500
tomnatt <github@tomnatt.com>
2025-02-24 14:30:11 +0000

    Refactor db add methods for DRYness

--- _lib/db_control.rb
+++ _lib/db_control.rb
@@ -13,26 +13,24 @@ class DbControl
   end
 
   def self.add_all_pictures
+    add_pictures(Config.year_range)
+  end
+
+  def self.update
+    # Eg range 2025 -> 2025
+    add_pictures(Config.latest_year..Config.latest_year)
+  end
+
+  def self.add_pictures(year_range)
     db = SQLite3::Database.open Config.database_path
 
     # Read each source file and add pictures to db
-    Config.year_range.each do |year|
+    year_range.each do |year|
       pics_data = YAML.load_file(Config.source_file_from_year(year))['pictures']
       pics_data.each do |pic_data|
         pic = Picture.new(pic_data, year)
         db.execute(pic.insert_sql, pic.values)
       end
-    end
-  end
-
-  def self.update
-    db = SQLite3::Database.open Config.database_path
-
-    # Read this year's source file and add pictures to db
-    pics_data = YAML.load_file(Config.source_file_from_year(Config.latest_year))['pictures']
-    pics_data.each do |pic_data|
-      pic = Picture.new(pic_data, Config.latest_year)
-      db.execute(pic.insert_sql, pic.values)
     end
   end
 end
