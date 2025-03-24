d91b8844add7015ac0ba5a863c26e130e4338e28
tomnatt <github@tomnatt.com>
2025-03-24 15:06:50 +0000

    Update test code for AI analysis

--- test.rb
+++ test.rb
@@ -4,9 +4,43 @@ require 'sqlite3'
 require 'yaml'
 require_relative '_lib/config'
 
+def output_filename(pic)
+  "#{File.basename(pic, '.jpg')}.yml"
+end
+
+def year_from_path(pic)
+  File.dirname(pic).split('/').last
+end
+
+def month_from_filename(pic)
+  m = File.basename(pic, '.jpg').split('-').first.to_i
+  Date::MONTHNAMES[m]
+end
+
+image_path = 'images/2025'
+
+pics = Dir["#{image_path}/*.jpg"]
+pics.each do |pic|
+  output_file = output_filename(pic)
+  year = year_from_path(pic)
+  month = month_from_filename(pic)
+
+  puts pic
+  puts "output file: #{output_file}"
+  puts "year:        #{year}"
+  puts "month:       #{month}"
+  puts "\n"
+end
+
+# The AI bit
+
 RubyLLM.configure do |config|
   config.openai_api_key = ENV.fetch('OPENAI_API_KEY_YIP')
 end
+
+prompt = 'Please defi'
+
+Please can you describe this photograph in around 200 words.
 
 prompt = 'Create a square thumbnail from the provided image,
 tightly cropped around the focal point. Center the thumbnail
@@ -17,12 +51,12 @@ or key object) while preserving balance and clarity.'
 # image = RubyLLM.paint
 
 # response = chat.ask "What's the difference between attr_reader and attr_accessor?"
-image_path = '/home/myst/Dropbox/pictures/year in pictures/2025/originals/02-tim_blair.jpg'
-thumbnail = RubyLLM.paint prompt, with: { image: image_path }
-thumbnail.save('myfile.png')
+# thumbnail = RubyLLM.paint prompt, with: { image: image_path }
+# thumbnail.save('myfile.png')
 
 # puts response.content
 
 # Generate an image
 # image = RubyLLM.paint('a sausage dressed in brightly coloured spandex jumping over a wall while running away from a dog, in a cartoon style')
 # image.save("myfile.png")
+
