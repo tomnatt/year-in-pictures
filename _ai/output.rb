f9d5b26bebf8b949d70e74817c0e270fa9263745
tomnatt <github@tomnatt.com>
2025-04-01 12:32:40 +0100

    Delete test code
    Moved to [open-ai-play](https://github.com/tomnatt/open-ai-play).

--- _ai/output.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-
-data_filepath = '_ai/data'
-keywords_file = "#{data_filepath}/keywords.json"
-existing_keywords = JSON.parse(File.read(keywords_file))
-
-out = "These are the keywords: #{existing_keywords.join(', ')}"
-
-puts out
