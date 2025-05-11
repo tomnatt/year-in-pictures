69dd4dc0e2108b5b72ce5af24c7ef33c2d039e56
tomnatt <github@tomnatt.com>
2025-05-11 14:31:16 +0100

    Partial implementation step

--- _lib/metadata.rb
+++ _lib/metadata.rb
@@ -1,22 +1,7 @@
+require 'active_record'
 require_relative 'config'
 
-class Metadata
-  def initialize(user_data)
-    @id = user_data['id']
-    @name = user_data['name']
-  end
-
-  # Add new or overwrite if already present
-  def insert_sql
-    'INSERT OR REPLACE INTO metadata (id,
-                                   name)
-      VALUES (?, ?)'
-  end
-
-  def values
-    [@id, @name]
-  end
-
+class Metadata < ActiveRecord::Base
   # Create year table
   def self.create_table_sql
     <<-SQL
@@ -25,7 +10,7 @@ class Metadata
         picture TEXT, # link to unique_name
         poem TEXT,
         ai_description TEXT,
-        keywords # TODO: join table
+        -- keywords # TODO: join table
       );
     SQL
   end
